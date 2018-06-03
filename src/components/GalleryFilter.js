1d1734f22db8053b029cab4a905894a174e424b5
Your Name <9leon1@gmail.com>
2018-06-03 18:03:05 +0300

    fixed styles

--- src/components/GalleryFilter.js
+++ src/components/GalleryFilter.js
@@ -36,23 +36,21 @@ export const GalleryFilter = props => {
                             {userSelected && <option value="rising">Rising</option>}
                         </select>
                     </div>
-
-                        <div className="filter__section">
-                            <label htmlFor="section">Time range</label>
-                            <select
-                                id="window"
-                                name="window"
-                                onChange={e => onFilterChange(e)}
-                                defaultValue={filterOptions.window}
-                            >
-                                <option value="day">Day</option>
-                                <option value="week">Week</option>
-                                <option value="month">Month</option>
-                                <option value="year">Year</option>
-                                <option value="all">All</option>
-                            </select>
-                        </div>
-
+                    <div className="filter__section">
+                        <label htmlFor="section">Time range</label>
+                        <select
+                            id="window"
+                            name="window"
+                            onChange={e => onFilterChange(e)}
+                            defaultValue={filterOptions.window}
+                        >
+                            <option value="day">Day</option>
+                            <option value="week">Week</option>
+                            <option value="month">Month</option>
+                            <option value="year">Year</option>
+                            <option value="all">All</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-
-//
-// import "./css/reset.css";
-// import "./css/framework.css";
-// import "./css/app.css";
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
